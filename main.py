dac7e705f5f9ca922ccc37f12539c523081efce4
Angel Ormaechea <104819851+AngelOrmaechea@users.noreply.github.com>
2023-02-15 17:41:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+url = "https://api.github.com"
+
+@app.route("/api/users", methods = ["GET"])
+def get_user():
+    since = request.args.get("since")
+    base = f"{url}/users?since={since}"
+    response = requests.get(base)
+    return jsonify({
+        'users': response.json(),
+        'next_page_link': response.headers.get('Link')
+    }
+    )
+
+@app.route("/api/users/<username>/details", methods = ["GET"])
+def Get_user_details(username):
+    base = f"{url}/users/{username}"
+    response = requests.get(base)
+    return jsonify( {
+       "user_details" : response.json()
+    })
+        
+
+
+@app.route("/api/users/<username>/repos", methods =["GET"])
+def Get_user_repos(username):
+    base = f"{url}/users/{username}/repos"
+    response = requests.get(base)
+    return jsonify(
+        {
+            "repos" : response.json() 
+        }
+    )
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test_main.py
@@ -0,0 +1,40 @@
+import unittest
+import requests
+
+
+class TestGHApi(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = "http://localhost:5000/api"
+
+
+    def test_users(self):
+        base = f"{self.base_url}/users"
+        response = requests.get(base)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("users" in response.json())
+        self.assertTrue("next_page_link" in response.json()) 
+
+
+    def test_get_user_details(self):
+        username = "octocat"
+        base = f"{self.base_url}/users/{username}/details"
+        response = requests.get(base)
+        self.assertEqual(response.status_code, 200)
+    
+
+    def test_get_user_repos(self):
+        username = "octocat"
+        base = f"{self.base_url}/users/{username}/repos"
+        response = requests.get(base)
+        self.assertEqual(response.status_code, 200)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+""" para lograr que se ejecute esto, en la terminal de VSC ejecute esto: 
+.\__pycache__\__pycache__\main.cpython-310.pyc
+se abrira una ventana que ejecuta el servidor y de ahi ejecutas esta ventana y corre el test"""
